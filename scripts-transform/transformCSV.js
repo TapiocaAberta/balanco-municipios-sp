5f872350dd442e7de61ba011d8fc238855425ef1
William Antonio <william.fatecsjc@gmail.com>
2016-04-11 04:22:29 -0300

    mexendo no arquivo de transformação

--- scripts-transform/transformCSV.js
+++ scripts-transform/transformCSV.js
@@ -1,13 +1,15 @@
 var csv = require("fast-csv"), normalizer = require('diacritics');
 
-var municipios = {};
-/* * First we need to write a JSON will cities human readable name and a normalized lower case name. *  * This JSON will be written to: *  * APP_ROOT/data/municipios/municipios.json *  * This data is taken from the expenses JSON *  */
+var municipiosMapa = {};
+/* * First we need to write a JSON will cities human readable name and a normalized lower case name. *  * This JSON will be written to: *  * APP_ROOT/data/municipiosMapa/municipiosMapa.json *  * This data is taken from the expenses JSON *  */
 csv
  .fromPath("./data/despesa-funcao-subfunc.csv", {delimiter: ';' })
- .on("data", function(data){     municipios[normalizer.remove(data[2])] = data[2] 
+ .on("data", function(data){     var id = normalizer.remove(data[2]).toLowerCase().split(' ').join('_')     municipiosMapa[id] = data[2] 
  })
-  .on("end", function(){	console.log(municipios)
-    });
+  .on("end", function(){    var municipios = [];    for(m in municipiosMapa) {          municipios.push({              id: m,
+              nome: municipiosMapa[m]          });    }
+    // TODO: escrever JSON, msa agora presico domirr, amanhá eh labuta tiozao    console.log(municipios)
+ });
 
 /* * Will read the expenses data and write a JSON with the expenses of a given year to: *  * APP_ROOT/data/despesas/{year}/{normalized lower case city name}.json * csv
  .fromPath("./data/despesa-funcao-subfunc.csv", {delimiter: ';' })
