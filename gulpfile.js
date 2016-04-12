cd69f2130b713145a0de96ae7fcf3d28b02d95a2
William Antonio <william.fatecsjc@gmail.com>
2016-04-12 03:30:33 -0300

    configuring project

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
-var gulp = require('gulp')
-  ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean')
-  ,concat = require('gulp-concat')
-  ,htmlReplace = require('gulp-html-replace')
-  ,uglify = require('gulp-uglify')
-  ,usemin = require('gulp-usemin')
-  ,cssmin = require('gulp-cssmin')
-  ,browserSync = require('browser-sync')
-  ,jshint = require('gulp-jshint')
-  ,jshintStylish = require('jshint-stylish')
-  ,csslint = require('gulp-csslint')
-  ,autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp');
+require('es6-promise').polyfill();
+// Include plugins
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'browser-sync', 'jshint-stylish'],
+	replaceString: /\bgulp[\-.]/
+});
+
 
 gulp.task('default', ['copy'], function() {
     gulp.start('build-img', 'usemin');
@@ -23,27 +18,27 @@ gulp.task('copy', ['clean'], function() {
 
 gulp.task('clean', function() {
     return gulp.src('dist')
-        .pipe(clean());
+        .pipe(plugins.clean());
 });
 
 gulp.task('build-img', function() {
 
   return gulp.src('dist/images/**/*')
-    .pipe(imagemin())
+    .pipe(plugins.imagemin())
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('usemin', function() {
   return gulp.src('dist/**/*.html')
-    .pipe(usemin({
-      js: [uglify],
-      css: [autoprefixer,cssmin]
+    .pipe(plugins.usemin({
+      js: [plugins.uglify],
+      css: [plugins.autoprefixer,plugins.cssmin]
     }))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('server', function() {
-    browserSync.init({
+    plugins.browserSync.init({
         server: {
             baseDir: 'src'
         }
@@ -54,15 +49,15 @@ gulp.task('server', function() {
     gulp.watch('app/js/**/*.js').on('change', function(event) {
         console.log("Linting " + event.path);
         gulp.src(event.path)
-            .pipe(jshint())
-            .pipe(jshint.reporter(jshintStylish));
+            .pipe(plugins.jshint())
+            .pipe(plugins.jshint.reporter(jshintStylish));
     });
 
     gulp.watch('app/css/**/*.css').on('change', function(event) {
         console.log("Linting " + event.path);
         gulp.src(event.path)
-            .pipe(csslint())
-            .pipe(csslint.reporter());
+            .pipe(plugins.csslint())
+            .pipe(plugins.csslint.reporter());
     });    
 
 });
